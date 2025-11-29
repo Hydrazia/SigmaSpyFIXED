9e42c33fff54b3de61861865ddad23e4dd152d5e
BoostCC <djejhjejr@gmail.com>
2025-11-29 05:33:48 -0500

    Remove old comments and add new header

--- Main.lua
+++ Main.lua
@@ -22,7 +22,7 @@ local Configuration = {
 	UseWorkspace = false, 
 	NoActors = false,
 	RepoUrl = "https://raw.githubusercontent.com/BoostCC/SigmaSpyFIXED/refs/heads/main",
-	ParserUrl = "https://raw.githubusercontent.com/depthso/Roblox-parser/refs/heads/main/"
+	ParserUrl = "https://raw.githubusercontent.com/depthso/Roblox-parser/refs/heads/main/",
 }
 
 --// Load overwrites
@@ -135,3 +135,4 @@ local ActorCode = Files:MakeActorScript(Scripts, ChannelId)
 Hook:LoadHooks(ActorCode, ChannelId)
 
 print("Sigma Spy loaded successfully with asset error bypass!")
+
